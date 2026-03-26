cd59c6a657fbf8bd83d434d9941035382f6bfbdc
Walt Sellers <wsellers@omnissa.com>
2026-03-26 12:40:17 -0700

    initial setup for test

---
+++ LICENSE.md
@@ -0,0 +1,8 @@
+## License
+
+This software is licensed under the [Omnissa Software Development Kit (SDK) License Agreement](https://static.omnissa.com/sites/default/files/omnissa-sdk-agreement.pdf); you may not use this software except in compliance with the License.
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+This software may also utilize Third-Pary Open Source Software as detailed on the [Open Source Notices](https://www.omnissa.com/open-source-notices/) page.
+
---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:5.7
+
+//
+//  Package.swift
+//  Workspace ONE Intelligence SDK
+//
+//  Copyright (c) 2023-2026 Omnissa, LLC. All rights reserved.
+//  This product is protected by copyright and intellectual property laws in the
+//  United States and other countries as well as by international treaties.
+//  -- Omnissa Public
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "WS1IntelligenceSDK",
+    platforms: [.iOS(.v16)],
+    products: [
+        .library(
+            name: "WS1IntelligenceSDK",
+            targets: ["OmnissaCryptoSDK", "WS1IntelligenceSDK"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/euc-releases/ws1-crypto-sdk.git", from: "25.06.0")
+    ],
+    targets: [
+        .binaryTarget(name: "WS1IntelligenceSDK", url: "https://github.com/WWSellers/euc-releases--ws1-intelligencesdk-sdk-ios/releases/download/26.2.0/WS1IntelligenceSDK.xcframework.zip", checksum: "e574f4cd7ed2fc9a309206c0bf73bfa82a153b0fb6f6e7309795f7ce525cb6d5"),
+        .target(name: "OmnissaCryptoSDK", dependencies: [.product(name: "WS1CryptoSDK", package: "ws1-crypto-sdk")])
+    ]
+)
